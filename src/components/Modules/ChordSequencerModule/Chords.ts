f7b847da70796ec1dfab1a294e3c40491e78ece0
Daniil Zorin <boolentfv@gmail.com>
2025-06-18 20:49:21 +0300

    refactor: remove code duplication

--- src/components/Modules/ChordSequencerModule/Chords.ts
+++ src/components/Modules/ChordSequencerModule/Chords.ts
@@ -5,30 +5,17 @@ import { useKeys } from "../PianoModule/useKeys";
 export const useChords = () => {
     const CHORD_OCTAVE = 5;
     const keys = useKeys();
-    const chords = ref<Chord[]>([...generateMajorChords(), ...generateMinorChords()]);
-    function generateMajorChords() {
+    const chords = ref<Chord[]>([...generateChords([4,7], (rootKey) => rootKey ), ...generateChords([3,7], (rootKey) => `${rootKey}m`)]);
+
+    function generateChords(notesSchema: number[], buildChordName: (rootKey: string) => string) {
         const firstOctave = keys.value.filter(key => key.octave === CHORD_OCTAVE);
         const firstOctaveIndex = keys.value.findIndex(key => key.octave === CHORD_OCTAVE);
         const defaultKey = firstOctave[0];
-        return firstOctave.map((key, index) => ({
-            name: key.key,
+        return firstOctave.map((rootNote, index) => ({
+            name: buildChordName(rootNote.key),
             tones: [
-                key,
-                keys.value ? keys.value[firstOctaveIndex + index + 4] : defaultKey,
-                keys.value ? keys.value[firstOctaveIndex + index + 7] : defaultKey
-            ]
-        }));
-    }
-    function generateMinorChords() {
-        const firstOctave = keys.value.filter(key => key.octave === CHORD_OCTAVE);
-        const firstOctaveIndex = keys.value.findIndex(key => key.octave === CHORD_OCTAVE);
-        const defaultKey = firstOctave[0];
-        return firstOctave.map((key, index) => ({
-            name: `${key.key}m`,
-            tones: [
-                key,
-                keys.value ? keys.value[firstOctaveIndex + index + 3] : defaultKey,
-                keys.value ? keys.value[firstOctaveIndex + index + 7] : defaultKey
+                rootNote,
+                ...notesSchema.map(offset => keys.value ? keys.value[firstOctaveIndex + index + offset] : defaultKey)
             ]
         }));
     }
